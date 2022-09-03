dfe5854a5375274cdc45c324268fe6c0cb13512e
Breydi Ramos Terrones <67590524+XxProFetxX@users.noreply.github.com>
2022-09-03 17:30:40 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <header-item></header-item>
+    <task-list v-bind:tasks="tasks" v-on:delete-task="deleteTask"></task-list>
+    <add-task v-on:add-task="addTask"></add-task>
+    <footer-item></footer-item>
+  </div>
+</template>
+
+<script>
+  import TaskList from "./components/task-list.component.vue"
+  import AddTask from "@/components/add-task.component.vue";
+  import HeaderItem from "./components/common/header-item.component.vue"
+  import FooterItem from "./components/common/footer-item.component.vue"
+  export default {
+    name: "App",
+    components: {FooterItem, HeaderItem, AddTask, TaskList},
+    data() {
+      return {
+        tasks: [
+          { id: 1, title: "Prepare Final Project Status Report", completed: false},
+          { id: 2, title: "Study English", completed: false},
+          { id: 3, title: "Do Laundry", completed: false},
+          { id: 4, title: "Cook breakfast", completed: false},
+          { id: 5, title: "Learn Vue", completed: false},
+        ]
+      }
+    },
+    methods: {
+      addTask(newTask) {
+        this.tasks = [...this.tasks, newTask]
+      },
+      deleteTask(taskId) {
+        this.tasks = this.tasks.filter(task => task.id !== taskId)
+      }
+    }
+  }
+</script>
+
+<style>
+  .containerXD {
+    flex: 2;
+  }
+</style>
---
+++ src/components/common/footer-item.component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Derechos reservados ©acme.com - 20 de febrero del 2022</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer-item.component"
+}
+</script>
+
+<style scoped>
+
+</style>
